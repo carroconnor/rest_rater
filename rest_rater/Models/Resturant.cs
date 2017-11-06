8a4db5eecc1ca03eea5dcce3925c66765c6a341d
Carr OConnor <carr.oconnor@gmail.com>
2017-11-06 11:13:49 -0500

    rest rater initial push

---
+++ rest_rater/Models/Resturant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace rest_rater.Models
+{
+    public class Resturant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDBContext : DbContext
+    {
+        public DbSet<Resturant> Restaurants { get; set; }
+    }
+}
